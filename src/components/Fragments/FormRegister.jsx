46f7d98bbf398997afdb40e9a85a0726ef85ebc0
dinowidyap15 <dinowidyap15@gmail.com>
2024-12-26 16:59:21 +0700

    fix: dropdown arrow

--- src/components/Fragments/FormRegister.jsx
+++ src/components/Fragments/FormRegister.jsx
@@ -20,7 +20,7 @@ const FormRegister = () => {
           <option value="wanita">Wanita</option>
           <option value="pria">Pria</option>
         </select>
-        <div className="absolute inset-y-0 right-0 flex items-center pt-7 pr-4 pointer-events-none text-dark-2">
+        <div className="absolute inset-y-0 right-2 pt-7 flex items-center pointer-events-none">
           <DropdownArrow />
         </div>
       </div>
@@ -29,18 +29,20 @@ const FormRegister = () => {
         <label htmlFor="gender" className="flex items-center gap-1 font-lato font-regular text-dark-2 sm:text-md text-sm">
           No. Hp <span className="font-poppins text-red-500">*</span>
         </label>
-        <div className=" flex space-x-2 gap-4">
-          <select
-            id="country"
-            name="country"
-            className="font-lato !sm:text-md w-1/3 mt-2 h-12 px-4 border border-bg-border rounded-lg
-      text-dark-1 focus:outline-none focus:ring-1 focus:ring-gr-300 appearance-none"
-          >
-            <option value="ID">+62</option>
-            <option value="US">+1</option>
-          </select>
-          <div className="absolute inset-y-0 left-0 flex items-center pt-7 sm:pl-28 pl-20 pointer-events-none text-dark-2">
-            <DropdownArrow />
+        <div className="flex space-x-2 gap-4">
+          <div className="relative w-1/3">
+            <select
+              id="country"
+              name="country"
+              className="font-lato !sm:text-md w-full mt-2 h-12 px-4 border border-bg-border rounded-lg
+        text-dark-1 focus:outline-none focus:ring-1 focus:ring-gr-300 appearance-none"
+            >
+              <option value="ID">+62</option>
+              <option value="US">+1</option>
+            </select>
+            <div className="absolute inset-y-0 right-2 pt-2 flex items-center pointer-events-none">
+              <DropdownArrow />
+            </div>
           </div>
           <input type="tel" className="font-lato w-2/3 mt-2 h-12 px-4 border border-bg-border rounded-lg text-dark-1 focus:outline-none focus:ring-1 focus:ring-gr-300" />
         </div>
