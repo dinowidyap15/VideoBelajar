e8cb5a6cf127b57444c943ef1ffa19d116260010
dinowidyap15 <dinowidyap15@gmail.com>
2024-12-26 12:47:58 +0700

    fix: error page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
 
   return (
     <section
-      className="flex max-w-full mx-auto xl:pt-16 lg:pt-14 md:pt-12 pt-8 xl:px-32 lg:px-20 md:px-16 px-6 gap-8
+      className="flex max-w-full min-h-screen mx-auto xl:pt-16 lg:pt-14 md:pt-12 pt-8 xl:px-32 lg:px-20 md:px-16 px-6 gap-8
      bg-bg-main xl:pb-16 lg:pb-14 md:pb-12 pb-8 justify-center"
     >
       <div className="relative font-poppins text-2xl flex flex-col justify-center text-center">
