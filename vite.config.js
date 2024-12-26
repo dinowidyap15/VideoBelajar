5fc9785c9109570c687b54a6b5843520e4041abf
dinowidyap15 <dinowidyap15@gmail.com>
2024-12-26 12:18:42 +0700

    fix: vercel routing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Output untuk build
+  },
+  server: {
+    port: 3000, // Port lokal untuk development
+  },
+  resolve: {
+    alias: {
+      "@": "/src", // Alias untuk folder src
+    },
+  },
+});
